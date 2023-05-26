560785b8f1d69b2015c807115d68bc538df91569
Cxr0l <vanerubio2020@gmail.com>
2023-05-26 15:55:47 -0400

    se agrego el js en la fecha de vencimiento

--- src/container.js
+++ src/container.js
@@ -5,9 +5,27 @@ inputNombreTarjeta.addEventListener('input', function() {
     cardName.textContent = inputNombreTarjeta.value;
 });
 
+
+
+
 const inputNumerosTarjeta = document.getElementById('numeros-tarjeta');
 const cardNumber = document.querySelector('.card-number');
 
 inputNumerosTarjeta.addEventListener('input', function() {
     cardNumber.textContent = inputNumerosTarjeta.value;
+});
+
+
+
+const inputMes = document.getElementById('cardinput-mes');
+const inputAño = document.getElementById('cardinput-año');
+const spanMes = document.getElementById('card-mes');
+const spanAño = document.getElementById('card-año');
+
+inputMes.addEventListener('input', function() {
+    spanMes.textContent = inputMes.value;
+});
+
+inputAño.addEventListener('input', function() {
+    spanAño.textContent = inputAño.value;
 });
