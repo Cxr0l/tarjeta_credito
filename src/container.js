e2f0f2052c31cc6ff1d7532eeb57b9c67e27372d
Cxr0l <vanerubio2020@gmail.com>
2023-05-26 23:20:51 -0400

    se termino los errores

--- src/container.js
+++ src/container.js
@@ -8,7 +8,22 @@ inputNombreTarjeta.addEventListener('input', function() {
     } else {
         cardName.textContent = inputNombreTarjeta.value;
     }
+    verificarLetras();
 });
+
+
+const errorHolder = document.querySelector('.error-holder');
+
+function verificarLetras() {
+    const nombre = inputNombreTarjeta.value;
+    if (/[^A-Za-zñÑáéíóúÁÉÍÓÚ\s]/.test(nombre)) {
+        errorHolder.textContent = 'Solo se permiten letras';
+    } else {
+        errorHolder.textContent = '';
+    }
+}
+
+
 
 
 
@@ -16,13 +31,31 @@ const inputNumerosTarjeta = document.getElementById('numeros-tarjeta');
 const cardNumber = document.querySelector('.card-number');
 const numerosOriginales = cardNumber.textContent;
 
+//PARA QUE SE VUELVA A RELLENAR EL ESPACIO AL ESTAR VACIO
 inputNumerosTarjeta.addEventListener('input', function() {
     if (inputNumerosTarjeta.value === '') {
         cardNumber.textContent = numerosOriginales;
     } else {
         cardNumber.textContent = inputNumerosTarjeta.value;
     }
+    verificarNumerosTarjeta();
 });
+
+//ERROR DE LOS NUMEROS TARJETA
+const errorNumero = document.querySelector('.error-number');
+
+function verificarNumerosTarjeta() {
+    const numeroTarjeta = inputNumerosTarjeta.value;
+    if (/[^0-9]+/.test(numeroTarjeta)) {
+        errorNumero.textContent = 'Solo se permiten números';
+    } else {
+        errorNumero.textContent = '';
+    }
+}
+
+
+
+
 
 const inputMes = document.getElementById('cardinput-mes');
 const inputAño = document.getElementById('cardinput-año');
@@ -39,6 +72,7 @@ inputMes.addEventListener('input', function() {
     } else {
         spanMes.textContent = inputMes.value;
     }
+    verificarNumeros();
 });
 
 inputAño.addEventListener('input', function() {
@@ -47,7 +81,23 @@ inputAño.addEventListener('input', function() {
     } else {
         spanAño.textContent = inputAño.value;
     }
+    verificarNumeros();
 });
+
+const errorMes = document.querySelector('.error-mes');
+
+function verificarNumeros() {
+    const mes = inputMes.value;
+    const año = inputAño.value;
+    if (/[^0-9]/.test(mes) || /[^0-9]/.test(año)) {
+        errorMes.textContent = 'Solo se permiten números';
+    } else {
+        errorMes.textContent = '';
+    }
+}
+
+
+
 
 
 const inputCVV = document.getElementById('card-CVV');
@@ -60,4 +110,16 @@ inputCVV.addEventListener('input', function() {
     } else {
         cvv.textContent = inputCVV.value;
     }
+    verificarNumerosCVV();
 });
+
+const errorNumber = document.querySelector('.error-CVV');
+
+function verificarNumerosCVV() {
+    const numeroCVV = inputCVV.value;
+    if (/[^0-9]+/.test(numeroCVV)) {
+        errorNumber.textContent = 'Solo se permiten números';
+    } else {
+        errorNumber.textContent = '';
+    }
+}
